3c970f21adbbd996335f643200d40e5a9a0f04b5
ddimkou <ddimkou@outlook.com>
2024-03-12 12:24:45 +0200

    initial form

--- src/pages/Contact/Contact.tsx
+++ src/pages/Contact/Contact.tsx
@@ -1,4 +1,4 @@
-import { Container, Paper } from "@mui/material";
+import { Box, Container, Paper, TextField, Typography } from "@mui/material";
 
 const Contact = () => {
   return (
@@ -11,7 +11,35 @@ const Contact = () => {
           backgroundColor: "rgba(20, 20, 20, 0.8)",
         }}
       >
-        x
+        <Typography variant="h5" sx={{ marginBottom: "1em", color: "#ff8c00" }}>
+          Contact form
+        </Typography>
+        <Box
+          component="form"
+          sx={{
+            "& .MuiTextField-root": { m: 1, width: "40em" },
+          }}
+          noValidate
+          autoComplete="off"
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+        >
+          <TextField
+            id="outlined-multiline-flexible"
+            label="Name:"
+            multiline
+            maxRows={4}
+          />
+          <TextField id="outlined-textarea" label="Email:" multiline />
+          <TextField
+            id="outlined-multiline-static"
+            label="Your Message:"
+            multiline
+            rows={6}
+            defaultValue=""
+          />
+        </Box>
       </Paper>
     </Container>
   );
--- src/theme.ts
+++ src/theme.ts
@@ -2,8 +2,53 @@ import { createTheme } from "@mui/material/styles";
 
 const theme = createTheme({
   palette: {
-    secondary: {
+    primary: {
       main: "#ff8c00",
+    },
+    secondary: {
+      main: "#ff9a1f",
+    },
+  },
+  components: {
+    // LABEL
+    MuiInputLabel: {
+      styleOverrides: {
+        root: {
+          color: "var(--accordion-text)",
+          "&.Mui-focused": {
+            color: "var(--accent-color)",
+          },
+        },
+      },
+    },
+    // TEXTFIELD
+    MuiTextField: {
+      styleOverrides: {
+        root: {
+          "& .MuiInput-underline:after": {
+            borderBottomColor: "var(--accent-color)",
+          },
+          "& .MuiOutlinedInput-root": {
+            "& fieldset": {
+              borderColor: "var(--accordion-text)",
+            },
+            "&:hover fieldset": {
+              borderColor: "var(--accordion-text)",
+            },
+            "&.Mui-focused fieldset": {
+              borderColor: "var(--accent-color)",
+            },
+          },
+        },
+      },
+    },
+    // Input
+    MuiInputBase: {
+      styleOverrides: {
+        input: {
+          color: "var(--accordion-text)",
+        },
+      },
     },
   },
 });
